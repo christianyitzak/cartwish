ea3e6282aa0145e280989f67eaed1394527a0e68
Christian Yitzak <christianyitzak@gmail.com>
2026-09-11 09:22:34 +0700

    setup express and mongodb on backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log("MongoDB Connection failed..", err))
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
