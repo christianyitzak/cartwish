a6951d9dcf5ea6f32e4ad579cf6931f5e3bab663
Christian Yitzak <christianyitzak@gmail.com>
2026-09-11 14:11:52 +0700

    add joi validation on user register route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,24 @@
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
 
+const createUserSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  deliveryAddress: Joi.string().min(5).required()
+});
+
 router.post("/", async (req, res) => {
   const { name, email, password, deliveryAddress } = req.body;
+
+  const joiValidation = createUserSchema.validate(req.body);
+
+  if (joiValidation.error) {
+    return res.status(400).json(joiValidation.error.details[0].message);
+  };
 
   const user = await User.findOne({ email });
 
